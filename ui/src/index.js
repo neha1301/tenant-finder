bbc5fb3a64195f661bba449d7aecb86c262d5c8b
Dinesh Khoja <Dinesh.khoja@outlook.com>
2023-04-17 20:33:03 +0530

    added auth provider to project

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import './App.css';
-import reportWebVitals from './reportWebVitals';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "./App.css";
+import reportWebVitals from "./reportWebVitals";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import { AuthProvider } from "./context/authContext"
+import { AuthProvider } from "./context/authContext";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  
   <React.StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
-    
   </React.StrictMode>
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
